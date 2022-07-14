1bb94894d6b54549fc4d28757f483f128e0d5f54
Evandro Petrus <evandro_petrus@outlook.com>
2022-07-14 09:09:00 -0300

    Project started based on Goodrich-Tamassia's Data Struchture and Algorithms

--- src/src/com/company/Main.java
+++ src/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package src.com.company;
-import java.util.Arrays;
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int[] numberArray = {10, 20, 30, 40}; // goes from position 0 up to position "i-1"("n-1").
+    int i = 0;
+      while (i<=3) {
+          System.out.println(numberArray[i]);
+          i++;
+      }
     }
 }
+
